87610d5a5c5bccdd86feb9f024b6817d7f0235fa
turalmz <turalmz@gmail.com>
2026-05-18 22:48:44 +0400

    Update main.py simplified

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import sys
 from PySide6.QtWidgets import QApplication
 from ui import MainWindow
 
-app = QApplication(sys.argv)
 
-window = MainWindow()
-window.show()
+def main():
+    app = QApplication(sys.argv)
 
-sys.exit(app.exec())
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
